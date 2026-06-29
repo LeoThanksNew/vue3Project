ab9237e526f5f1adfdee8ee130a46d7c8a752a93
Leo <18872840541@139.com>
2026-06-29 19:33:28 +0800

    添加测试文件

---
+++ vue-project/src/components/home.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="home-page">
+        <h1>{{ count }}</h1>
+        <h1>{{ testObj.name }}</h1>
+        <h1>{{ doubleCount }}</h1>
+        <button @click="increment">Increment</button>
+        <button @click="updateName">Update Name</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive, computed } from 'vue'
+
+const count = ref(0)
+const increment = () => {
+    count.value++
+}
+
+const testObj = reactive({
+    name: 'John',
+    age: 30
+})
+const updateName = () => {
+    testObj.name = 'TestJane' + Math.random().toString(36).substring(7)
+}
+
+// 计算属性
+const doubleCount = computed(() => {
+    return count.value + testObj.name
+})
+</script>
